d8888077270b75130bdfc8a030276f53f052c3c6
devmarcosv <devmarcosvinicius@gmail.com>
2024-12-05 14:11:06 -0300

    fix layout

--- app/fichas/page.tsx
+++ app/fichas/page.tsx
@@ -2,26 +2,28 @@
 
 import { useEffect, useState } from "react";
 
-interface User {
-  id: number;
-  name: string;
-  password: string;
+interface Ficha {
+  id: string;
+  user_id: string;
+  nome: string;
+  created_at: string;
+  updated_at: string;
 }
 
 const CardsPage = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const [fichas, setFichas] = useState<Ficha[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchFichas = async () => {
       try {
         const response = await fetch("http://localhost:3333/fichas");
         if (!response.ok) {
-          throw new Error("Erro ao buscar usuários");
+          throw new Error("Erro ao buscar fichas");
         }
         const data = await response.json();
-        setUsers(data);
+        setFichas(data);
       } catch (err: any) {
         setError(err.message);
       } finally {
@@ -29,7 +31,7 @@ const CardsPage = () => {
       }
     };
 
-    fetchUsers();
+    fetchFichas();
   }, []);
 
   if (loading) return <p className="text-center">Carregando...</p>;
@@ -53,19 +55,27 @@ const CardsPage = () => {
           <thead className="text-xs text-gray-700 uppercase bg-purple-600">
             <tr>
               <th className="px-6 py-3">Nome</th>
-              <th className="px-6 py-3">Senha</th>
+              <th className="px-6 py-3">ID do Usuário</th>
+              <th className="px-6 py-3">Criado em</th>
+              <th className="px-6 py-3">Atualizado em</th>
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
+            {fichas.map((ficha) => (
               <tr
-                key={user.id}
+                key={ficha.id}
                 className="bg-gray-50 border-b hover:bg-gray-100"
               >
                 <td className="px-6 py-4 font-medium text-purple-700">
-                  {user.name}
+                  {ficha.nome}
                 </td>
-                <td className="px-6 py-4 text-purple-700">{user.password}</td>
+                <td className="px-6 py-4 text-purple-700">{ficha.user_id}</td>
+                <td className="px-6 py-4 text-purple-700">
+                  {new Date(ficha.created_at).toLocaleString()}
+                </td>
+                <td className="px-6 py-4 text-purple-700">
+                  {new Date(ficha.updated_at).toLocaleString()}
+                </td>
               </tr>
             ))}
           </tbody>
